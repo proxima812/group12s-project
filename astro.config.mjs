d2ab2d726e53b6a194465ecb9e6466846138dc4b
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-09-09 09:17:06 +0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
+import webVitals from "@astrojs/web-vitals";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind(), react()],
+  integrations: [db(), tailwind(), react(), webVitals()],
   output: "server",
   adapter: vercel()
 });
