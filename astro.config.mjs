c016d2f32bc8942e58617a9ff42bec675f81b9e7
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-09-13 18:04:41 +0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@ import db from "@astrojs/db"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
-// import vercel from "@astrojs/vercel/serverless";
-
 import webVitals from "@astrojs/web-vitals"
-
 import sitemap from "@astrojs/sitemap"
 
-import cloudflare from "@astrojs/cloudflare"
+
+import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [db(), tailwind(), react(), webVitals(), sitemap()],
-	site: "https://12step.vercel.app/",
-	output: "server",
-	adapter: cloudflare(),
+  integrations: [db(), tailwind(), react(), webVitals(), sitemap()],
+  site: "https://12step.vercel.app/",
+  output: "server",
+  adapter: vercel(),
 })
