0959be0ea0e3e39f02648a72208c46bb9cea94c2
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-09-19 17:07:41 +0500

    12321

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import db from "@astrojs/db"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
-import webVitals from "@astrojs/web-vitals"
 import { defineConfig } from "astro/config"
 
 import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [db(), tailwind(), react(), webVitals(), sitemap()],
+	integrations: [db(), tailwind(), react(), sitemap()],
 	site: "https://12step.vercel.app/",
 	output: "server",
 	adapter: netlify(),
